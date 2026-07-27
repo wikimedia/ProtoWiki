1ffdb69e31e2843ec5bdf3c75b63305ce6a9e7d1
TodePond <TodePond@users.noreply.github.com>
2026-07-27 16:54:28 +0000

    Deploy preview for PR 59 🛫

---
+++ pr-preview/pr-59/assets/index-jKvEGGJ6.js
@@ -0,0 +1,1 @@
+import{P as o}from"./PlainWrapper-IhNmP_3o.js";import{d as a,s as r,o as t,w as l,a as n}from"./index-htyR9oxo.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=a({__name:"index",setup(s){return(p,e)=>(t(),r(o,{heading:"Hello world"},{default:l(()=>[...e[0]||(e[0]=[n("p",null,"Hello, world!",-1)])]),_:1}))}});export{_ as default};
