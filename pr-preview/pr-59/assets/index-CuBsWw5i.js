33080a23ccc46f069daa7540932ee4b9d05995c3
TodePond <TodePond@users.noreply.github.com>
2026-07-28 11:33:19 +0000

    Deploy preview for PR 59 🛫

---
+++ pr-preview/pr-59/assets/index-CuBsWw5i.js
@@ -0,0 +1,1 @@
+import{P as a}from"./PlainWrapper-DO2J2bx8.js";import{d as r,s as t,w as p,o as n,a as o}from"./index-DnJL-5G3.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=r({__name:"index",setup(s){return(i,e)=>(n(),t(a,{heading:"Plain wrapper"},{default:p(()=>[...e[0]||(e[0]=[o("p",null,"Starter template — empty plain wrapper.",-1)])]),_:1}))}});export{_ as default};
