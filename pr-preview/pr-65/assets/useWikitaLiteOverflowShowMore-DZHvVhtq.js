385d1aef437b2bd0ee3a68dd72aea8589e26151e
TodePond <TodePond@users.noreply.github.com>
2026-09-09 14:26:48 +0000

    Deploy preview for PR 65 🛫

--- pr-preview/pr-65/assets/useWikitaLiteOverflowShowMore-DZHvVhtq.js
+++
@@ -1,1 +0,0 @@
-import{u as o,b as t}from"./useWikitaLiteCardListClasses-b8eTbQVH.js";import{s as i,a0 as a}from"./index-DQn96IcS.js";function l(e){const{useModuleMenuMode:r}=o(),{hideTabBar:u}=t();return i(()=>a(e.requireHideTabBar)&&!u.value?!1:!a(e.standalone)&&r.value&&!!a(e.moreTo)&&a(e.hasItems))}export{l as u};
